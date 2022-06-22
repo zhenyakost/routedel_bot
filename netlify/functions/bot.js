4ed83a66b76fcd5fc4e02330c205f2be57843d23
Женя Костовецкий <zhenyakost@MacBook-Pro-Zena.local>
2022-06-23 00:23:24 +0300

    ad

--- netlify/functions/bot.js
+++ netlify/functions/bot.js
@@ -13,37 +13,36 @@ const app = initializeApp(firebaseConfig);
 
 const database = getDatabase(app);
 
-bot.start((ctx) =>
-  ctx.reply(`Привіт, це сервіс OrderFood 😋
-Замовляй товари онлайн та отримуй їх у місці де є кур’єр.`)
-);
-
-bot.on('pre_checkout_query', (ctx) => ctx.answerPreCheckoutQuery(true)) // ответ на предварительный запрос по оплате
-
-bot.on('successful_payment', async (ctx, next) => {
-  let order_id = 0;
-  const snapshot = await get(child(ref(database), 'orders/'));
-  if(snapshot.exists()){
-    const obj = snapshot.val();
-    order_id = Object.keys(obj).reverse().find(a => obj[a].user_id === ctx.chat.id)
-  }
-  await ctx.reply(`Номер замовлення ${order_id}.
-Далі просто підійдіть до кур’єра з цим номером та отримайте своє замовлення 😌`)
-  .then(get(child(ref(database), `orders/${order_id}`)).then(snapshot => {
-    const obj = snapshot.val();
-    axios.post(`https://api.telegram.org/bot${TOKEN}/sendMessage`, JSON.stringify({
-    'chat_id':'-1001609321370',
-    'text':`Номер замовлення: <b>${order_id}</b>
-Імʼя: ${obj.user_name},
-Замовлення:
-   ${obj.goods.map((a, i) => `${a} x${obj.quant[i]} \n   `).join('')}`,
-   'parse_mode':'html'
-  }), {headers: {'Content-Type':'application/json'}})}))
-})
-
-bot.launch();
-
 exports.handler = async (event) => {
-    console.log("Received an update from Telegram!", event.body);
+    bot.start((ctx) =>
+    ctx.reply(`Привіт, це сервіс OrderFood 😋
+    Замовляй товари онлайн та отримуй їх у місці де є кур’єр.`)
+    );
+
+    bot.on('pre_checkout_query', (ctx) => ctx.answerPreCheckoutQuery(true)) // ответ на предварительный запрос по оплате
+
+    bot.on('successful_payment', async (ctx, next) => {
+    let order_id = 0;
+    const snapshot = await get(child(ref(database), 'orders/'));
+    if(snapshot.exists()){
+        const obj = snapshot.val();
+        order_id = Object.keys(obj).reverse().find(a => obj[a].user_id === ctx.chat.id)
+    }
+    await ctx.reply(`Номер замовлення ${order_id}.
+    Далі просто підійдіть до кур’єра з цим номером та отримайте своє замовлення 😌`)
+    .then(get(child(ref(database), `orders/${order_id}`)).then(snapshot => {
+        const obj = snapshot.val();
+        axios.post(`https://api.telegram.org/bot${TOKEN}/sendMessage`, JSON.stringify({
+        'chat_id':'-1001609321370',
+        'text':`Номер замовлення: <b>${order_id}</b>
+    Імʼя: ${obj.user_name},
+    Замовлення:
+    ${obj.goods.map((a, i) => `${a} x${obj.quant[i]} \n   `).join('')}`,
+    'parse_mode':'html'
+    }), {headers: {'Content-Type':'application/json'}})}))
+    })
+
+    bot.launch();
+        
     return { statusCode: 200 };
-  };
+};
